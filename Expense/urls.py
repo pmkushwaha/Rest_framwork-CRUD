5800318850047d1c61595e431a6d0679f16f3fa1
pmkushwaha <premmohankushwaha2001abc@gmail.com>
2026-02-13 01:56:27 +0530

    Post and Patch method added

--- Expense/urls.py
+++ Expense/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import ExpenseApi
+from .views import ExpenseApi , ExpenseUpApi
+ 
+ 
 
 
 urlpatterns = [
     path('expense',ExpenseApi.as_view(), name='ExpenseApi'),
+ path('expense/<int:id>/',ExpenseUpApi.as_view(),name='ExpenseUpApi'),
  
 ]
--- Expense/views.py
+++ Expense/views.py
@@ -10,8 +10,30 @@ class ExpenseApi(APIView):
 
     def get(self,request):
         data=Expanse.objects.all()
-        serializer=ExpanseSerializer(data  ,many=True) 
+        serializer=ExpanseSerializer(data,many=True) 
         return Response(serializer.data )
    
+
+    def post(self ,request):
+        serializer=ExpanseSerializer(data=request.data )
+        if serializer.is_valid():
+            serializer.save()
+            return  Response( {"Message ": "Data saved successfully"})
     
-    
+
+class ExpenseUpApi(APIView):
+
+    def get(self,request,id):
+        data=Expanse.objects.get(id=id)
+        serializer=ExpanseSerializer(data)
+        return Response(serializer.data)
+             
+    def patch(self ,request, id ):
+        item=Expanse.objects.get(id=id)
+        serialiser=ExpanseSerializer(item,data=request.data, partial = True)
+        if serialiser.is_valid():
+            serialiser.save()
+            return Response({"Message":"Message Updated successfully"})
+        else:
+            return Response(serialiser.errors)
+
